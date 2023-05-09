aa7ed4a6987dfffcf00626d9a09fd990bde65104
João Paulo <132070590+Joao-Paulo-Brito@users.noreply.github.com>
2023-05-09 18:42:30 -0300

    09/05/2023

--- ex014.js/ex014.js
+++ ex014.js/ex014.js
@@ -1,15 +1,21 @@
-// Variáveis
-var agora = new Date()
-var hora = agora.getHours()
-var resposta = window.document.getElementById('res')
+// Função
+function carregar(){
+    // Variáveis
+    var agora = new Date()
+    var hora = agora.getHours()
+    var resposta = window.document.getElementById('res')
+    var img = window.document.querySelector('foto')
+    resposta.innerText = `Agora são ${hora} horas`
 
-resposta.innerText = `Agora são ${hora} horas`
-
-// Condições
-if(hora < 12){
-    alert("Bom dia!")
-}else if(hora < 18){
-    alert("Boa tarde!")
-}else{
-    alert("Boa noite!")
+    // Condições
+    if(hora < 12){
+        alert("Bom dia!")
+        img.src = 'Manhã.png'
+    }else if(hora < 18){
+        alert("Boa tarde!")
+        img.src = 'tarde.png'
+    }else{
+        alert("Boa noite!")
+        img.src = 'img/noite.png'
+    }
 }
