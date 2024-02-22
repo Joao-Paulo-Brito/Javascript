3bf5045e9dfce9170d4c07995aa19722f52f167c
João Paulo <132070590+Joao-Paulo-Brito@users.noreply.github.com>
2024-02-22 17:53:52 -0300

    22/02/2024

--- ex019.js/script/ex019.js
+++ ex019.js/script/ex019.js
@@ -3,20 +3,26 @@ var vet = []
 
 function adicionar(){
     // Variáveis
-    var numero = document.getElementById('number').value
+    var resposta = document.getElementById('res');
+    var numero = Number(document.getElementById('number').value)
     var tab = document.getElementById('tab')
     var item = document.createElement('option')
 
+    resposta.innerHTML = ''
+
     // Validação
-    if(numero.length == 0){
+    if(numero === 0){
         window.alert('Digite um número');
     }else if(numero < 1 || numero > 100){
         item.text = 'Errado, digite um número entre 1 e 100'
         tab.appendChild(item)
     }else{
+        // Remover aviso de erro
         while(tab.lastChild.textContent === 'Errado, digite um número entre 1 e 100'){
             tab.removeChild(tab.lastChild)
         }
+
+        // Adicionando valores
         vet.push(numero)
         item.text = `Número ${numero} adicionado`
         tab.appendChild(item)
@@ -28,16 +34,64 @@ function finalizar(){
     var resposta = document.getElementById('res');
     var frase = document.createElement('p');
 
+    // Variáveis para os valores do vetor
+    var tamanho = vet.length
+    var maior = Math.max(...vet)
+    let menor = Math.min(...vet)
+    let soma = vet.reduce((total, numero) => total + parseInt(numero), 0)
+    // Reduce tem a mesma função do for, para somar os valores do vetor, estabelecendo duas variáveis que vão ser somandas, o "total" que é o valor anterior dentro do vetor que será iterado com o "numero", que é o valor atual dentro do vetor, sendo que o "total" inicia com valor 0
+    /* É a mesma coisa de:
+        var soma = 0;
+        for(contador=0;contador<vet.lengh;contador++){
+            vet[contador] += soma
+        }
+    */
+   var media = soma/tamanho
+
     // Apagar conteúdo depois de clicar
     resposta.innerHTML = ''
 
-    // Tamanho do vetor
-    if(vet.length == 1){
-        /*text Content é todo o texto que será exibido dentro do elemento p no Html*/
-        frase.textContent = `Ao todo, existe apenas ${vet.length} número`;
-        resposta.appendChild(frase);
-    } else {
-        frase.textContent = `Ao todo existem ${vet.length} números`;
-        resposta.appendChild(frase);
+    // Validação
+    if(vet.length == 0){
+        window.alert('Digite um número');
+    }else{
+        // Tamanho do vetor
+        if(vet.length == 1){
+            /*text Content é todo o texto que será exibido dentro do elemento p no Html*/
+            frase.innerHTML = `Ao todo, existe apenas ${tamanho} número<br>` +
+                            `Maior valor do vetor: ${maior}<br>` +
+                            `Menor valor do vetor: ${menor}<br>` +
+                            `A soma de todos os valores é: ${soma}<br>` +
+                            `A média dos valores do vetor é: ${media}`;
+            resposta.appendChild(frase);
+        } else {
+            // Saída de dados
+            frase.innerHTML = `Ao todo, existem ${tamanho} números<br>` +
+                            `Maior valor do vetor: ${maior}<br>` +
+                            `Menor valor do vetor: ${menor}<br>` +
+                            `A soma de todos os valores é: ${soma}<br>` +
+                            `A média dos valores do vetor é: ${media}`;
+            resposta.appendChild(frase);
+        }
     }
+}
+
+function apagar(){
+    // Variáveis
+    var resposta = document.getElementById('res');
+    var numero = Number(document.getElementById('number').value)
+    var tab = document.getElementById('tab')
+
+    // Validação
+    if(numero === 0){
+        window.alert("Digite um número")
+    }else{
+        // Deletar texto
+        tab.innerHTML = ''
+        resposta.innerHTML = ''
+
+        // Apagando os elementos do vetor
+        vet = []
+    }
+
 }
